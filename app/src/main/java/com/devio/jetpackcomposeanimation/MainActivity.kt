0c3b619068e6dcb9996a9603ead2b30f6562fcc5
vibhuti shah <vibhuti.shah@mutualmobile.com>
2022-03-27 12:47:49 +0530

    Initial commit

---
+++ app/src/main/java/com/devio/jetpackcomposeanimation/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.devio.jetpackcomposeanimation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.devio.jetpackcomposeanimation.ui.theme.JetpackComposeAnimationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeAnimationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+//                    Greeting("Android")
+                    ComposeOneScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackComposeAnimationTheme {
+        Greeting("Android")
+    }
+}
